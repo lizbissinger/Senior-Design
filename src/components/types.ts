6c4b3660f5b06474220ce53024f78ddeb6f52541
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2023-10-04 16:34:44 -0400

    #19 Added a starting point for invoices

---
+++ src/components/types.ts
@@ -0,0 +1,22 @@
+export type LoadDetail = {
+      loadNumber: string;
+      truckObject: string;
+      trailerObject: string;
+      driverObject: string;
+      pickupTime: string;
+      deliveryTime: string;
+      documents: string;
+      price: string;
+      detention: string;
+      allMiles: string;
+      gallons: string;
+      status: string;
+      brokerInfo: {
+        name: string;
+        phoneNumber: string;
+        email: string;
+        company: string;
+      };
+      comments: string;
+  };
+  
