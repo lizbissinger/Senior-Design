961f895aa8865649756dc3ba33ecd7abcb7889a4
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2023-10-31 23:52:33 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Overview from './components/Overview/Overview';
-import FleetManagement from './components/FleetManagement/FleetManagement';
-import LoadDetails from './components/LoadDetails/LoadDetails'; 
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Overview from "./components/Overview/Overview";
+import FleetManagement from "./components/FleetManagement/FleetManagement";
+import LoadDetails from "./components/LoadDetails/LoadDetails";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/dashboard/*" element={<Dashboard />} />
           <Route index element={<Dashboard />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
           <Route path="/overview" element={<Overview />} />
           <Route path="/fleet" element={<FleetManagement />} />
-
         </Routes>
       </Router>
     </div>
