301767ee633d8cafa9e2a12cdbe45cbee9a4f56b
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2024-01-25 15:06:05 -0500

    Fixed routing for driverDetails

--- src/routes/driverDetails.tsx
+++ src/routes/driverDetails.tsx
@@ -2,20 +2,23 @@ import { DriverDetail } from "../components/Types/types";
 
 const api = import.meta.env.VITE_API_URL;
 
-async function GetAllDrivers() {
-    let drivers;
-    await fetch(`${api}/driverDetails`, {
-        method: "GET"
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        drivers = data;
-    })
-    .catch((error) => console.log(error));
-    return drivers;
+async function GetAllDrivers(): Promise<DriverDetail[] | undefined> {
+  try {
+    const response = await fetch(`${api}/driverDetails`, {
+      method: "GET"
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch drivers: ${response.statusText}`);
+    }
+
+    const data: DriverDetail[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
 }
-
-
 // export async function CreateNewLoad(load: DriverDetail) {
 //     let newLoad;
 //     const requestOptions = {
