2c855d0e77de5525a0ae68d879a42ddf23a66aa6
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2024-01-25 20:59:34 -0500

    Updated Trailer Details Route

--- src/routes/trailerDetails.tsx
+++ src/routes/trailerDetails.tsx
@@ -2,7 +2,7 @@ import { TrailerDetail } from "../components/Types/types";
 
 const api = import.meta.env.VITE_API_URL;
 
-async function GetAllTrailers(): Promise<TrailerDetail[] | undefined> {
+export async function GetAllTrailers(): Promise<TrailerDetail[] | undefined> {
   try {
     const response = await fetch(`${api}/trailerDetails`, {
       method: "GET"
@@ -20,5 +20,70 @@ async function GetAllTrailers(): Promise<TrailerDetail[] | undefined> {
   }
 }
 
+export async function CreateNewTrailer(trailer: TrailerDetail): Promise<TrailerDetail | undefined> {
+  try {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(trailer),
+    };
+
+    const response = await fetch(`${api}/trailerDetails`, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`Failed to create a new trailer: ${response.statusText}`);
+    }
+
+    const newTrailer: TrailerDetail = await response.json();
+    return newTrailer;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+export async function DeleteTrailer(id: string): Promise<TrailerDetail | undefined> {
+  try {
+    const response = await fetch(`${api}/trailerDetails/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to delete the trailer: ${response.statusText}`);
+    }
+
+    const deletedTrailer: TrailerDetail = await response.json();
+    return deletedTrailer;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+export async function UpdateTrailer(trailer: TrailerDetail): Promise<TrailerDetail | undefined> {
+  try {
+    const requestOptions = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(trailer),
+    };
+
+    const response = await fetch(`${api}/trailerDetails/${trailer._id}`, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`Failed to update the trailer: ${response.statusText}`);
+    }
+
+    const updatedTrailer: TrailerDetail = await response.json();
+    return updatedTrailer;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
 
 export default GetAllTrailers;
