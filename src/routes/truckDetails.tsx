a3a88bf781f0cb1f3cd22e9dcfdcf9ce876a4fd9
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2024-01-25 21:00:09 -0500

    Updated Truck Details route

--- src/routes/truckDetails.tsx
+++ src/routes/truckDetails.tsx
@@ -2,7 +2,7 @@ import { TruckDetail } from "../components/Types/types";
 
 const api = import.meta.env.VITE_API_URL;
 
-async function GetAllTrucks(): Promise<TruckDetail[] | undefined> {
+export async function GetAllTrucks(): Promise<TruckDetail[] | undefined> {
   try {
     const response = await fetch(`${api}/truckDetails`, {
       method: "GET"
@@ -20,5 +20,70 @@ async function GetAllTrucks(): Promise<TruckDetail[] | undefined> {
   }
 }
 
+export async function CreateNewTruck(truck: TruckDetail): Promise<TruckDetail | undefined> {
+  try {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(truck),
+    };
+
+    const response = await fetch(`${api}/truckDetails`, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`Failed to create a new truck: ${response.statusText}`);
+    }
+
+    const newTruck: TruckDetail = await response.json();
+    return newTruck;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+export async function DeleteTruck(id: string): Promise<TruckDetail | undefined> {
+  try {
+    const response = await fetch(`${api}/truckDetails/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to delete the truck: ${response.statusText}`);
+    }
+
+    const deletedTruck: TruckDetail = await response.json();
+    return deletedTruck;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+export async function UpdateTruck(truck: TruckDetail): Promise<TruckDetail | undefined> {
+  try {
+    const requestOptions = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(truck),
+    };
+
+    const response = await fetch(`${api}/truckDetails/${truck._id}`, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`Failed to update the truck: ${response.statusText}`);
+    }
+
+    const updatedTruck: TruckDetail = await response.json();
+    return updatedTruck;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
 
 export default GetAllTrucks;
