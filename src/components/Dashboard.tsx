9cd5fd347cc3246ed3c30bfcbdc67d83f8d885f4
sekhonsj <sekhonsj@mail.uc.edu>
2024-01-28 13:20:24 -0500

    removed commented out navbar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -163,37 +163,6 @@ const Dashboard: React.FC = () => {
         </Routes>
       </div>
     </div>
-    // <div className="dashboard">
-    //   <div className="top-bar">
-    //   <h1>FLEETWAVE</h1>
-    //     <div className="tabs">
-    //       <Link to="/dashboard/overview" className="tab">
-    //         Overview
-    //       </Link>
-    //       <Link to="/dashboard/fleet" className="tab">
-    //         Fleet Management
-    //       </Link>
-    //       <Link to="/dashboard/reports" className="tab">
-    //         Reports
-    //       </Link>
-    //       <Link to="/dashboard/finance" className="tab">
-    //         Finance
-    //       </Link>
-          
-    //       {/* Add more tabs for other features */}
-    //     </div>
-    //     <UserInfo username="Jatt Singh" /> {/* Pass the user's name */}
-    //   </div>
-    //   <div className="content">
-    //     <Routes>
-    //       <Route path="overview" element={<Overview />} />
-    //       <Route path="fleet" element={<FleetManagement />} />
-    //       <Route path="reports" element={<Reports/>} />
-    //       <Route path="finance" /* add element here */ />
-    //       {/* Add more routes for other tabs */}
-    //     </Routes>
-    //   </div>
-    // </div>
   );
 };
 
