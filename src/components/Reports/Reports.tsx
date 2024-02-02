5dbd0128cc129d9861f1ff651affbc6b64fdb974
sekhonsj <sekhonsj@mail.uc.edu>
2024-02-01 21:39:48 -0500

    connected reports tab with backend and fixed the logo

--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -6,49 +6,68 @@ import {
   Title,
   DateRangePicker,
   SearchSelect,
-  SearchSelectItem
+  SearchSelectItem,
+  Divider
 } from "@tremor/react";
 import { useState } from "react";
 import{ UserIcon } from "@heroicons/react/24/solid";
+import GetAllDrivers from "../../routes/driverDetails";
+import GetAllRevenueReports from '../../routes/reports';
+import { GetDriverRevenueReports } from '../../routes/reports';
 
 const Reports: React.FC = () => {
   const [driver, setDriver] = useState("");
-  const chartdata = [
-    {
-      date: "Jan 22",
-      SemiAnalysis: 2890,
-      "The Pragmatic Engineer": 2338,
-    },
-    {
-      date: "Feb 22",
-      SemiAnalysis: 2756,
-      "The Pragmatic Engineer": 2103,
-    },
-    {
-      date: "Mar 22",
-      SemiAnalysis: 3322,
-      "The Pragmatic Engineer": 2194,
-    },
-    {
-      date: "Apr 22",
-      SemiAnalysis: 3470,
-      "The Pragmatic Engineer": 2108,
-    },
-    {
-      date: "May 22",
-      SemiAnalysis: 3475,
-      "The Pragmatic Engineer": 1812,
-    },
-    {
-      date: "Jun 22",
-      SemiAnalysis: 3129,
-      "The Pragmatic Engineer": 1726,
-    },
-  ];
+  const [drivers, setDrivers] = useState<string[]>([]);
+  const [areaChartData, setAreaChartData] = useState([]);
+  const [categories, setCategories] = useState<string[]>(["Cumulative"]);
   
   const valueFormatter = function (number:number) {
     return "$ " + new Intl.NumberFormat("us").format(number).toString();
   };
+
+  const fetchAllDrivers = async () => {
+    try {
+      const driverList = await GetAllDrivers();
+
+      if (driverList) {
+        const driverNames = driverList.map((driver) => driver.name);
+        setDrivers(driverNames);
+      }
+    } catch (error) {}
+  };
+
+  const fetchAllRevenueData = async () => {
+    try {
+      const allRevenueData = await GetAllRevenueReports();
+
+      if (allRevenueData) {
+        setAreaChartData(allRevenueData);
+        setCategories(["Cumulative"]);
+      }
+    } catch (error) {}
+  };
+
+  const fetchDriverRevenueData = async () => {
+    try {
+      const driverRevenueData = await GetDriverRevenueReports(driver);
+
+      if (driverRevenueData) {
+        console.log(driverRevenueData);
+        setAreaChartData(driverRevenueData);
+        setCategories([driver]);
+      }
+    } catch (error) {}
+  };
+
+  // handle driver filter change
+  useEffect(() => {
+    if (driver.length > 0) {
+      fetchDriverRevenueData();
+    } else {
+      fetchAllDrivers();
+      fetchAllRevenueData();
+    }
+  }, [driver]);
 
   return (
     <div>
@@ -57,28 +76,35 @@ const Reports: React.FC = () => {
         <DateRangePicker className="max-w-sm mx-auto" enableSelect={false} />
         <div className="max-w-sm mx-auto space-y-6">
           <SearchSelect value={driver} onValueChange={setDriver} placeholder="Filter by driver" icon={UserIcon}>
-            <SearchSelectItem value="Raj" icon={UserIcon}>
-            Raj
-            </SearchSelectItem>
-            <SearchSelectItem value="Rash" icon={UserIcon}>
-            Rash
-            </SearchSelectItem>
-            <SearchSelectItem value="Jazzy" icon={UserIcon}>
-            Jazzy
-            </SearchSelectItem>
-            <SearchSelectItem value="Rishabh" icon={UserIcon}>
-            Rishabh
-            </SearchSelectItem>
+            {drivers.map((d) => (
+              <SearchSelectItem key={d} value={d} icon={UserIcon} />
+            ))}
           </SearchSelect>
         </div>
         <DateRangePicker className="max-w-sm mx-auto" enableSelect={false} />
       </Grid>
+      <Divider>Charts</Divider>
       <Grid numItemsMd={2} numItemsLg={2} className="gap-6 mt-6">
         <Card>
+            <Title>Revenue Over Time</Title>
+            <AreaChart
+              className="h-72 mt-4"
+              data={areaChartData}
+              index="date"
+              yAxisWidth={65}
+              categories={categories}
+              colors={["indigo"]}
+              valueFormatter={valueFormatter}
+              showAnimation={true}
+              animationDuration={1500}
+              curveType="monotone"
+            />
+          </Card>
+          <Card>
             <Title>Newsletter revenue over time (USD)</Title>
             <AreaChart
               className="h-72 mt-4"
-              data={chartdata}
+              data={areaChartData}
               index="date"
               yAxisWidth={65}
               categories={["SemiAnalysis", "The Pragmatic Engineer"]}
@@ -90,7 +116,7 @@ const Reports: React.FC = () => {
             <Title>Newsletter revenue over time (USD)</Title>
             <AreaChart
               className="h-72 mt-4"
-              data={chartdata}
+              data={areaChartData}
               index="date"
               yAxisWidth={65}
               categories={["SemiAnalysis", "The Pragmatic Engineer"]}
@@ -102,19 +128,7 @@ const Reports: React.FC = () => {
             <Title>Newsletter revenue over time (USD)</Title>
             <AreaChart
               className="h-72 mt-4"
-              data={chartdata}
-              index="date"
-              yAxisWidth={65}
-              categories={["SemiAnalysis", "The Pragmatic Engineer"]}
-              colors={["indigo", "cyan"]}
-              valueFormatter={valueFormatter}
-            />
-          </Card>
-          <Card>
-            <Title>Newsletter revenue over time (USD)</Title>
-            <AreaChart
-              className="h-72 mt-4"
-              data={chartdata}
+              data={areaChartData}
               index="date"
               yAxisWidth={65}
               categories={["SemiAnalysis", "The Pragmatic Engineer"]}
