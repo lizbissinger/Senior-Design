454f489c24acc9c8455219503f7232d209d04772
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2024-02-08 00:06:21 -0500

    adding login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Overview from './components/Overview/Overview';
-import FleetManagement from './components/FleetManagement/FleetManagement';
-import LoadDetails from './components/LoadDetails/LoadDetails'; 
-import Reports from './components/Reports/Reports';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Overview from "./components/Overview/Overview";
+import FleetManagement from "./components/FleetManagement/FleetManagement";
+import LoadDetails from "./components/LoadDetails/LoadDetails";
+import Reports from "./components/Reports/Reports";
+import {
+  SignOutButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
+import SignInPage from "./routes/sign-in";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/dashboard/*" element={<Dashboard />} />
-          <Route index element={<Dashboard />} />
-          <Route path="/overview" element={<Overview />} />
-          <Route path="/fleet" element={<FleetManagement />} />
-          <Route path='/reports' element={<Reports />} />
-        </Routes>
-      </Router>
+      <SignedIn>
+        <UserButton afterSignOutUrl="/sign-in" />
+        <Router>
+          <Routes>
+            <Route path="/dashboard/*" element={<Dashboard />} />
+            <Route index element={<Dashboard />} />
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/fleet" element={<FleetManagement />} />
+            <Route path="/reports" element={<Reports />} />
+          </Routes>
+        </Router>
+      </SignedIn>
+      <SignedOut>
+        <Router>
+          <Routes>
+            <Route path="/sign-in" element={<SignInPage />} />
+          </Routes>
+        </Router>
+      </SignedOut>
     </div>
   );
 };
