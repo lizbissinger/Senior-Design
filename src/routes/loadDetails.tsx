34fc85c9babf5efe5d1cfbf651dc6ec303a6d517
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2024-02-21 17:34:32 -0500

    Updated create load to handle documents upload

--- src/routes/loadDetails.tsx
+++ src/routes/loadDetails.tsx
@@ -3,53 +3,66 @@ import { LoadDetail } from "../components/Types/types";
 const api = import.meta.env.VITE_API_URL;
 
 async function GetAllLoads() {
-    let loads;
-    await fetch(`${api}/loadDetails`, {
-        method: "GET"
-    })
+  let loads;
+  await fetch(`${api}/loadDetails`, {
+    method: "GET",
+  })
     .then((response) => response.json())
     .then((data) => {
-        loads = data;
+      loads = data;
     })
     .catch((error) => console.log(error));
-    return loads;
+  return loads;
 }
 
-export async function CreateNewLoad(load: LoadDetail) {
-    let newLoad;
-    const requestOptions = {
-        method: "POST",
-        headers: { 
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(load)
-    };
-    await fetch(`${api}/loadDetails`, requestOptions)
-        .then(response => response.json())
-        .then(data => newLoad = data);
-    return newLoad;
-}
+export async function CreateNewLoad(load: LoadDetail, files?: File[]) {
+  const formData = new FormData();
 
+  if (files) {
+    files.forEach((file) => {
+      formData.append("documents", file);
+    });
+  }
+  formData.append("load", JSON.stringify(load));
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    body: formData,
+  };
+
+  try {
+    const response = await fetch(`${api}/loadDetails`, requestOptions);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error creating new load:", error);
+    throw error;
+  }
+}
 export async function DeleteLoad(id: string) {
-    let deletedLoad;
-    await fetch(`${api}/loadDetails/${id}`, {
-        method: "DELETE"
-    })
-        .then(response => response.json())
-        .then(data => deletedLoad = data);
-    return deletedLoad;
+  let deletedLoad;
+  await fetch(`${api}/loadDetails/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => (deletedLoad = data));
+  return deletedLoad;
 }
 
 export async function UpdateLoad(load: LoadDetail) {
-    const requestOptions = {
-        method: "PATCH",
-        headers: { 
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(load)
-    };
-    return await fetch(`${api}/loadDetails/${load._id}`, requestOptions)
-        .then(response => response.json())
+  const requestOptions = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(load),
+  };
+  return await fetch(`${api}/loadDetails/${load._id}`, requestOptions).then(
+    (response) => response.json()
+  );
 }
 
 export default GetAllLoads;
