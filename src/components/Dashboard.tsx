e69afb993bdce86bbb9de7c316b22d5d941b184b
rashminder2002 <rashminder2002@gmail.com>
2024-02-21 15:08:35 -0500

    Fixed header menu to close on tap in mobile view

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import {
+  Link,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import Overview from "./Overview/Overview";
 import FleetManagement from "./FleetManagement/FleetManagement";
 import "./Dashboard.css";
@@ -11,14 +17,14 @@ import { UserButton } from "@clerk/clerk-react";
 import DarkModeSwitcher from "./DarkModeSwitcher";
 
 const Dashboard: React.FC = () => {
-  const logoText = "FLEETWAVE"; 
+  const logoText = "FLEETWAVE";
 
   useEffect(() => {
-    
     localStorage.setItem("logoText", logoText);
   }, []);
 
   const [activeTab, setActiveTab] = useState("");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -51,169 +57,169 @@ const Dashboard: React.FC = () => {
     },
   ];
 
-  function handleLinkClick(item: any) {
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLinkClick = (item: any) => {
     setActiveTab(item.href.split("/").pop() || "");
     navigate(item.href);
-  }
 
-  function classNames(...classes: any) {
+    setIsMobileMenuOpen(false);
+  };
+
+  const classNames = (...classes: any) => {
     return classes.filter(Boolean).join(" ");
-  }
+  };
 
   return (
     <div>
-      <Disclosure as="nav" className="bg-slate-100 sticky top-0 z-50">
-        {({ open }) => (
-          <>
-            <div className="max-w-200xl px-2 sm:px-6 lg:px-8">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-neutral-950 hover:bg-[#6686DC] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                  <DarkModeSwitcher />
-                </div>
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                  <div className="flex flex-shrink-0 items-center">
-                    <h1 className="h-8 w-auto select-none logo-text">
-                      <Link
-                        to="/dashboard/overview"
-                        className="no-underline text-neutral-900 hover:text-neutral-900 hover:no-underline"
-                        onClick={() => {
-                          setActiveTab("Overview");
-                        }}
-                      >
-                        <span>{logoText}</span>
-                      </Link>
-                    </h1>
-                  </div>
-                  <div className="hidden sm:ml-6 sm:block mt-1">
-                    <div className="flex space-x-4">
-                      {navigation.map((item) => (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          onClick={() => {
-                            setActiveTab(item.name);
-                            navigate(item.href);
-                          }}
-                          className={classNames(
-                            item.current
-                              ? "bg-[#6686DC] text-neutral-950 no-underline hover:text-neutral-950"
-                              : "no-underline text-neutral-950 hover:bg-[#6686DC] hover:text-neutral-950 hover:no-underline",
-                            "rounded-md px-3 py-2 text-50 font-medium"
-                          )}
-                        >
-                          {item.name}
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-                <DarkModeSwitcher />
-
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-                  <button
-                    type="button"
-                    className="relative rounded-full bg-[#7392e9] p-1 text-neutral-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-900"
+      <div className="bg-slate-100 sticky top-0 z-50">
+        <div className="max-w-200xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-neutral-950 hover:bg-[#6686DC] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={handleMobileMenuToggle}
+              >
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                {isMobileMenuOpen ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
+              <DarkModeSwitcher />
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                <h1 className="h-8 w-auto select-none logo-text">
+                  <Link
+                    to="/dashboard/overview"
+                    className="no-underline text-neutral-900 hover:text-neutral-900 hover:no-underline"
+                    onClick={() => {
+                      setActiveTab("Overview");
+                    }}
                   >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <UserButton afterSignOutUrl="/" />
-                    </div>
-                    <Transition
-                      as={React.Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
+                    <span>{logoText}</span>
+                  </Link>
+                </h1>
+              </div>
+              <div className="hidden sm:ml-6 sm:block mt-1">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.href}
+                      onClick={() => handleLinkClick(item)}
+                      className={classNames(
+                        item.current
+                          ? "bg-[#6686DC] text-neutral-950 no-underline"
+                          : "no-underline text-neutral-950 hover:bg-[#6686DC] hover:no-underline hover:text-neutral-950",
+                        "rounded-md px-3 py-2 text-50 font-medium"
+                      )}
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "no-underline block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Your Profile
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "no-underline block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "no-underline block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Sign out
-                            </a>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
+                      {item.name}
+                    </Link>
+                  ))}
                 </div>
               </div>
             </div>
+            <DarkModeSwitcher />
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button
+                type="button"
+                className="relative rounded-full bg-[#7392e9] p-1 text-neutral-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-900"
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+                <Transition
+                  as={React.Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "no-underline block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Your Profile
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "no-underline block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "no-underline block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Sign out
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          </div>
+        </div>
 
-            <Disclosure.Panel className="sm:hidden">
-              <div className="space-y-1 px-2 pb-3 pt-2">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    to={item.href}
-                    onClick={() => {
-                      setActiveTab(item.name);
-                      navigate(item.href);
-                    }}
-                    className={classNames(
-                      item.current
-                        ? "bg-[#6686DC] text-neutral-950 no-underline"
-                        : "no-underline text-neutral-950 hover:bg-[#6686DC] hover:no-underline hover:text-neutral-950",
-                      "block rounded-md px-3 py-2 text-base font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+        <div
+          className={`${
+            isMobileMenuOpen ? "sm:hidden" : "hidden"
+          } transition-transform ease-in-out duration-300`}
+        >
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                to={item.href}
+                onClick={() => handleLinkClick(item)}
+                className={classNames(
+                  item.current
+                    ? "bg-[#6686DC] text-neutral-950 no-underline"
+                    : "no-underline text-neutral-950 hover:bg-[#6686DC] hover:no-underline hover:text-neutral-950",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="content">
         <Routes>
           <Route path="overview" element={<Overview />} />
