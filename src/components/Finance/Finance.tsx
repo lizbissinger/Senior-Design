d9ef57b8de3c92ca582c39bc308c3d2d624a4b9c
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2024-03-02 01:01:35 -0500

    De-cluttered, removed files, unused imports

--- src/components/Finance/Finance.tsx
+++ src/components/Finance/Finance.tsx
@@ -1,25 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Grid, Dialog,Title ,Button, DialogPanel } from "@tremor/react";
-import RepairTable from '../Finance-Columns/Repairs-table';
-import Example from '../RepairsForm/RepairsForm';
-import Form from '../RepairsForm/RepairsForm';
-import { PayrollDetail, RepairDetail, Fuel } from '../Types/types';
-import RepairsForm from '../RepairsForm/RepairsForm';
-import GetAllRepairs, { CreateNewRepair } from '../../routes/repairDetails';
-import PayrollForm from '../PayrollForm/PayrollForm';
-import PayrollTable from '../Finance-Columns/Payroll-table';
-import GetAllPayroll from '../../routes/payrollDetails';
-import GetAllFuelRows from '../../routes/fuel';
-import FuelTable from '../Finance-Columns/Fuel-table';
-import FuelForm from '../FuelForm/FuelForm';
+import React, { useEffect, useState } from "react";
+import { Card, Grid, Title } from "@tremor/react";
+import RepairTable from "../Finance-Columns/Repairs-table";
+import { PayrollDetail, RepairDetail, Fuel } from "../Types/types";
+import RepairsForm from "../RepairsForm/RepairsForm";
+import GetAllRepairs from "../../routes/repairDetails";
+import PayrollForm from "../PayrollForm/PayrollForm";
+import PayrollTable from "../Finance-Columns/Payroll-table";
+import GetAllPayroll from "../../routes/payrollDetails";
+import GetAllFuelRows from "../../routes/fuel";
+import FuelTable from "../Finance-Columns/Fuel-table";
+import FuelForm from "../FuelForm/FuelForm";
 
-const Finance: React.FC= () =>{
+const Finance: React.FC = () => {
   const [repairDetails, setRepairDetails] = useState<RepairDetail[]>([]);
   const [payrollDetail, setPayrollDetails] = useState<PayrollDetail[]>([]);
   const [fuel, setFuel] = useState<Fuel[]>([]);
 
-  // repairs
- const fetchRepairs = async () => {
+  const fetchRepairs = async () => {
     try {
       const repairs = await GetAllRepairs();
       if (repairs) {
@@ -32,20 +29,16 @@ const Finance: React.FC= () =>{
 
   const handleAddRepair = async (newRepairDetail: RepairDetail) => {
     try {
-      // Call your backend API to create a new repair
-      const newRepair = (newRepairDetail);
+      const newRepair = newRepairDetail;
 
       if (newRepair) {
-        // Add the new repair detail to the existing list
         setRepairDetails((prevDetails) => [...prevDetails, newRepair]);
       }
     } catch (error) {
       console.error(error);
-      // Handle error as needed
     }
   };
 
-  //-----Payroll---
   const fetchPayroll = async () => {
     try {
       const payroll = await GetAllPayroll();
@@ -59,16 +52,13 @@ const Finance: React.FC= () =>{
 
   const handleAddPayroll = async (newPayrollDetail: PayrollDetail) => {
     try {
-      // Call your backend API to create a new repair
-      const newPayroll = (newPayrollDetail);
+      const newPayroll = newPayrollDetail;
 
       if (newPayroll) {
-        // Add the new repair detail to the existing list
         setPayrollDetails((prevDetails) => [...prevDetails, newPayroll]);
       }
     } catch (error) {
       console.error(error);
-      // Handle error as needed
     }
   };
 
@@ -84,16 +74,13 @@ const Finance: React.FC= () =>{
   };
   const handleAddForm = async (newFuel: Fuel) => {
     try {
-      // Call your backend API to create a new repair
-      const newFuelRow = (newFuel);
+      const newFuelRow = newFuel;
 
       if (newFuelRow) {
-        // Add the new repair detail to the existing list
         setFuel((prevDetails) => [...prevDetails, newFuelRow]);
       }
     } catch (error) {
       console.error(error);
-      // Handle error as needed
     }
   };
 
@@ -103,31 +90,36 @@ const Finance: React.FC= () =>{
     fetchFuelRows();
   }, []);
 
-    return(
-      <div>
-        <Grid numItemsMd={4} numItemsLg={4} className="gap-6 mt-6">
-          <Card>
-            <Title>Repairs</Title>
-            <RepairsForm onSubmitRepair={handleAddRepair} />
-            <RepairTable repairDetails={repairDetails} fetchRepairs={fetchRepairs}/>
-          </Card>
-          <Card>
-            <Title>Payroll</Title>
-            <PayrollForm onSubmitPayroll={handleAddPayroll} />
-            <PayrollTable payrollDetail={payrollDetail} fetchPayroll={fetchPayroll}/>
-          </Card>
-          <Card>
-            <Title>Fuel</Title>
-            <FuelForm onSubmitFuel={handleAddForm} />
-            <FuelTable fuel={fuel} fetchFuel={fetchFuelRows}/>
-          </Card>
-          <Card>
-            <Title>Revenue</Title>
-
-          </Card>
-        </Grid>
-      </div>
-    );
-}
+  return (
+    <div>
+      <Grid numItemsMd={4} numItemsLg={4} className="gap-6 mt-6">
+        <Card>
+          <Title>Repairs</Title>
+          <RepairsForm onSubmitRepair={handleAddRepair} />
+          <RepairTable
+            repairDetails={repairDetails}
+            fetchRepairs={fetchRepairs}
+          />
+        </Card>
+        <Card>
+          <Title>Payroll</Title>
+          <PayrollForm onSubmitPayroll={handleAddPayroll} />
+          <PayrollTable
+            payrollDetail={payrollDetail}
+            fetchPayroll={fetchPayroll}
+          />
+        </Card>
+        <Card>
+          <Title>Fuel</Title>
+          <FuelForm onSubmitFuel={handleAddForm} />
+          <FuelTable fuel={fuel} fetchFuel={fetchFuelRows} />
+        </Card>
+        <Card>
+          <Title>Revenue</Title>
+        </Card>
+      </Grid>
+    </div>
+  );
+};
 
 export default Finance;
--- src/components/RepairsForm/RepairsForm.tsx
+++ src/components/RepairsForm/RepairsForm.tsx
@@ -1,22 +1,23 @@
-
 import React, { useState } from "react";
-import { Button, Card, Dialog, DialogPanel, Divider, TextInput, Title ,DatePicker, DatePickerValue } from "@tremor/react";
+import {
+  Button,
+  Dialog,
+  DialogPanel,
+  Divider,
+  TextInput,
+  Title,
+} from "@tremor/react";
 
-import TruckDropdown from "../TruckForm/TruckDropdown";
 import { RepairDetail } from "../Types/types";
 import { CreateNewRepair } from "../../routes/repairDetails";
-// const [trucks, setTrucks] = useState<string[]>([]);
 interface RepairsFormProps {
-    onSubmitRepair: (repairDetail: RepairDetail) => void;
-  }
-  
-  
-  
-export default function RepairsForm({ onSubmitRepair }: RepairsFormProps) {
- const [isOpen, setIsOpen] = React.useState(false);
-  
+  onSubmitRepair: (repairDetail: RepairDetail) => void;
+}
 
-const [repairDetail, setRepairDetail] = useState<Partial<RepairDetail>>({
+export default function RepairsForm({ onSubmitRepair }: RepairsFormProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const [repairDetail, setRepairDetail] = useState<Partial<RepairDetail>>({
     repair: "",
     truckObject: "",
     trailerObject: "",
@@ -29,19 +30,15 @@ const [repairDetail, setRepairDetail] = useState<Partial<RepairDetail>>({
     setRepairDetail((prevDetail) => ({ ...prevDetail, [name]: value }));
   };
 
-
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-   console.log("Creating new repair", repairDetail);
+    console.log("Creating new repair", repairDetail);
     const newRepair = await CreateNewRepair(repairDetail as RepairDetail);
 
     if (newRepair) {
-      // Update the parent component state or perform any other actions as needed
       onSubmitRepair(newRepair);
-      
-      // Clear the form
+
       setRepairDetail({
         repair: "",
         truckObject: "",
@@ -49,13 +46,11 @@ const [repairDetail, setRepairDetail] = useState<Partial<RepairDetail>>({
         repairDate: "",
         repairCost: "",
         repairComments: "",
-      }); 
+      });
 
-      // Close the dialog
       setIsOpen(false);
     }
   };
-
 
   return (
     <>
@@ -64,101 +59,96 @@ const [repairDetail, setRepairDetail] = useState<Partial<RepairDetail>>({
       </div>
       <Dialog open={isOpen} onClose={(val) => setIsOpen(val)} static={true}>
         <DialogPanel>
-       
-          
-    
           <Button variant="light" onClick={() => setIsOpen(false)}>
-              Close
-            </Button>
+            Close
+          </Button>
           <Title className="mb-3">Add Repair Form</Title>
           <Divider />
-    <form className="repairs-form" onSubmit={handleSubmit}>
-    <div className="col-span-full">
-      <label
-          htmlFor="repair"
-          className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
-        >
-          Repair Detail
-          <span className="text-red-500">*</span>
-        </label>
-    <TextInput
-          placeholder="Repair Detail"
-          type="text"
-          name="repair"
-          value={repairDetail.repair}
-          onChange={handleInputChange}
-          required
-        />
-       </div> 
-        <Divider></Divider>
-        <div className="col-span-full">
-        <label
-          htmlFor="repairCost"
-          className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
-        >
-          Cost
-          <span className="text-red-500">*</span>
-        </label>
-        <TextInput
-          placeholder="Cost"
-          type="text"
-          name="repairCost"
-          value={repairDetail.repairCost}
-          onChange={handleInputChange}
-          required
-        />
-        </div>
-        <Divider></Divider>
-        <label
-          htmlFor="repairDate"
-          className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
-        >
-          Date
-          <span className="text-red-500">*</span>
-        </label>
-        <div className="col-span-full">
-        <input 
-        id="pickupTime"
-        name="repairDate"
-        type="datetime-local"
-        placeholder="Repair Date"
-        value={repairDetail.repairDate}
-        onChange={handleInputChange}
-        />
-        </div>
-      
-         <Divider></Divider>
-        <TextInput
-          placeholder="Select Truck (Make This deopdown)"
-          type="text"
-          name="truckObject"
-          value={repairDetail.truckObject}
-          onChange={handleInputChange}
-        />
-        <Divider></Divider>
-        <TextInput
-          placeholder="Select Trailer (Make This deopdown)"
-          type="text"
-          name="trailerObject"
-          value={repairDetail.trailerObject}
-          onChange={handleInputChange}
-          />
-        <Divider></Divider>
-        <TextInput
-          placeholder="Comments"
-          type="text"
-          name="repairComments"
-          value={repairDetail.repairComments}
-          onChange={handleInputChange}
-          />
-  
-      <Button className= "mt-3" type="submit"  > 
-      {/* onClick={() => setIsOpen(false)}     -- To close form on creation*/}
-        Create
-      </Button>
-    </form>
-    
-       
+          <form className="repairs-form" onSubmit={handleSubmit}>
+            <div className="col-span-full">
+              <label
+                htmlFor="repair"
+                className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
+              >
+                Repair Detail
+                <span className="text-red-500">*</span>
+              </label>
+              <TextInput
+                placeholder="Repair Detail"
+                type="text"
+                name="repair"
+                value={repairDetail.repair}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <Divider></Divider>
+            <div className="col-span-full">
+              <label
+                htmlFor="repairCost"
+                className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
+              >
+                Cost
+                <span className="text-red-500">*</span>
+              </label>
+              <TextInput
+                placeholder="Cost"
+                type="text"
+                name="repairCost"
+                value={repairDetail.repairCost}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <Divider></Divider>
+            <label
+              htmlFor="repairDate"
+              className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
+            >
+              Date
+              <span className="text-red-500">*</span>
+            </label>
+            <div className="col-span-full">
+              <input
+                id="pickupTime"
+                name="repairDate"
+                type="datetime-local"
+                placeholder="Repair Date"
+                value={repairDetail.repairDate}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <Divider></Divider>
+            <TextInput
+              placeholder="Select Truck (Make This deopdown)"
+              type="text"
+              name="truckObject"
+              value={repairDetail.truckObject}
+              onChange={handleInputChange}
+            />
+            <Divider></Divider>
+            <TextInput
+              placeholder="Select Trailer (Make This deopdown)"
+              type="text"
+              name="trailerObject"
+              value={repairDetail.trailerObject}
+              onChange={handleInputChange}
+            />
+            <Divider></Divider>
+            <TextInput
+              placeholder="Comments"
+              type="text"
+              name="repairComments"
+              value={repairDetail.repairComments}
+              onChange={handleInputChange}
+            />
+
+            <Button className="mt-3" type="submit">
+              {/* onClick={() => setIsOpen(false)}     -- To close form on creation*/}
+              Create
+            </Button>
+          </form>
         </DialogPanel>
       </Dialog>
     </>
