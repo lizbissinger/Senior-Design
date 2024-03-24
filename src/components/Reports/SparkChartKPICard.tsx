7f07b79782bff8c6a21bfda79751e293f575bbbb
rashminder2002 <80933649+RecklessRash@users.noreply.github.com>
2024-03-24 14:07:02 -0400

    Added skeleton loading to sparkcharts in reports tab

--- src/components/Reports/SparkChartKPICard.tsx
+++ src/components/Reports/SparkChartKPICard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, AreaChart } from "@tremor/react";
+import { PresentationChartLineIcon } from "@heroicons/react/24/solid";
 
 interface SparkChartKPICardProps {
   kpiCardData: {
@@ -17,6 +18,14 @@ const SparkChartKPICard: React.FC<SparkChartKPICardProps> = ({
   chartData,
   categories,
 }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
@@ -24,37 +33,51 @@ const SparkChartKPICard: React.FC<SparkChartKPICardProps> = ({
   return (
     <Card className="p-1.5 bg-gray-50 rounded-xl shadow-xl min-h-52 dark:shadow-slate-950 dark:shadow-xl">
       <Card className="rounded-md min-h-full">
-        <p className="flex items-start justify-between">
-          <span className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {kpiCardData.value}
-          </span>
-          <span
-            className={classNames(
-              kpiCardData.changeType == "positive"
-                ? "bg-emerald-100 text-emerald-800 ring-emerald-600/10 dark:bg-emerald-400/10 dark:text-emerald-500 dark:ring-emerald-400/20"
-                : "bg-red-100 text-red-800 ring-red-600/10 dark:bg-red-400/10 dark:text-red-500 dark:ring-red-400/20",
-              "inline-flex items-center rounded-tremor-small px-2 py-1 text-tremor-label font-medium ring-1 ring-inset"
-            )}
-          >
-            {kpiCardData.change}
-          </span>
-        </p>
-        <p className="mt-1 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-          {kpiCardData.name}
-        </p>
-        <AreaChart
-          className="max-h-14"
-          data={chartData}
-          index="date"
-          categories={categories}
-          colors={kpiCardData.changeType === "positive" ? ["emerald"] : ["red"]}
-          showAnimation={true}
-          showGridLines={false}
-          showXAxis={false}
-          showYAxis={false}
-          showLegend={false}
-          animationDuration={1500}
-        />
+        {isLoading ? (
+          <div className="flex items-center justify-center h-40 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <PresentationChartLineIcon
+              className="w-10 h-10 text-gray-200 dark:text-gray-600"
+              aria-hidden="true"
+            />
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <>
+            <p className="flex items-start justify-between">
+              <span className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                {kpiCardData.value}
+              </span>
+              <span
+                className={classNames(
+                  kpiCardData.changeType === "positive"
+                    ? "bg-emerald-100 text-emerald-800 ring-emerald-600/10 dark:bg-emerald-400/10 dark:text-emerald-500 dark:ring-emerald-400/20"
+                    : "bg-red-100 text-red-800 ring-red-600/10 dark:bg-red-400/10 dark:text-red-500 dark:ring-red-400/20",
+                  "inline-flex items-center rounded-tremor-small px-2 py-1 text-tremor-label font-medium ring-1 ring-inset"
+                )}
+              >
+                {kpiCardData.change}
+              </span>
+            </p>
+            <p className="mt-1 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+              {kpiCardData.name}
+            </p>
+            <AreaChart
+              className="max-h-14"
+              data={chartData}
+              index="date"
+              categories={categories}
+              colors={
+                kpiCardData.changeType === "positive" ? ["emerald"] : ["red"]
+              }
+              showAnimation={true}
+              showGridLines={false}
+              showXAxis={false}
+              showYAxis={false}
+              showLegend={false}
+              animationDuration={1500}
+            />
+          </>
+        )}
       </Card>
     </Card>
   );
